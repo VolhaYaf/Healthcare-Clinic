2e7ae290411fb70919fd347f6fb5defaa4f8fa4b
Volha <134659098+VolhaYaf@users.noreply.github.com>
2023-08-19 12:20:23 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+const button = document.querySelector('#btn');
+
+button.addEventListener('click', calculateAmount);
+
+const buttonInsurance = document.querySelector('#insurance');
+buttonInsurance.addEventListener('click', showPercent);
+
+function showPercent(e){
+    e.preventDefault();
+    percent.style.display = 'block';
+}
+
+function calculateAmount(e){
+    e.preventDefault();
+
+    const bill = document.querySelector('#bill').value;
+    const percent = document.querySelector('#percent').value;
+
+    if (bill === ''){
+        Swal.fire({
+            title: 'Enter the invoice amount!',
+            showClass: {
+              popup: 'animate__animated animate__fadeInDown'
+            },
+            hideClass: {
+              popup: 'animate__animated animate__fadeOutUp'
+            }
+          })
+    }
+
+    else if (bill < 0.1){
+        Swal.fire({
+            title: 'The invoice amount should be higher than 0!',
+            showClass: {
+              popup: 'animate__animated animate__fadeInDown'
+            },
+            hideClass: {
+              popup: 'animate__animated animate__fadeOutUp'
+            }
+          })
+    }
+
+
+    let insurancePays = bill * percent;
+    console.log(insurancePays);
+
+    let clientPays = bill - insurancePays;
+    console.log(clientPays);
+
+    document.querySelector('#client').textContent = clientPays.toFixed(2);
+    document.querySelector('#insur').textContent = insurancePays.toFixed(2);
+}
